0a75333558909752751919a97a1c408c68eb7bc5
Yuriy Marin <yuriy.marin@gmail.com>
2020-09-24 16:42:58 +0300

    feat: Загрузка данных с сервера
    Механическое повторение разбора задания из лекции.

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -1,6 +1,7 @@
 import BoardView from "../view/board.js";
 import SortingView from "../view/sorting.js";
 import TaskListView from "../view/task-list.js";
+import LoadingView from "../view/loading.js";
 import NoTasksView from "../view/no-tasks.js";
 import LoadMoreButtonView from "../view/load-more-button.js";
 import TaskPresenter from "./task.js";
@@ -13,13 +14,15 @@ import {SortType, UpdateType, UserAction} from "../const.js";
 const TASKS_AMOUNT_PER_TIER = 8;
 
 export default class Board {
-  constructor(boardContainer, tasksModel, filtersModel) {
+  constructor(boardContainer, tasksModel, filtersModel, api) {
     this._tasksModel = tasksModel;
     this._filtersModel = filtersModel;
     this._boardContainer = boardContainer;
     this._renderedTaskCount = TASKS_AMOUNT_PER_TIER;
     this._currentSortType = SortType.DEFAULT;
     this._taskPresenter = {};
+    this._isLoading = true;
+    this._api = api;
 
     this._sortingComponent = null;
     this._loadMoreButtonComponent = null;
@@ -27,6 +30,7 @@ export default class Board {
     this._boardComponent = new BoardView();
     this._taskListComponent = new TaskListView();
     this._noTaskComponent = new NoTasksView();
+    this._loadingComponent = new LoadingView();
 
     this._handleViewAction = this._handleViewAction.bind(this);
     this._handleModelEvent = this._handleModelEvent.bind(this);
@@ -88,7 +92,9 @@ export default class Board {
   _handleViewAction(actionType, updateType, update) {
     switch (actionType) {
       case UserAction.UPDATE_TASK:
-        this._tasksModel.updateTask(updateType, update);
+        this._api.updateTask(update).then((response) => {
+          this._tasksModel.updateTask(updateType, response);
+        });
         break;
       case UserAction.ADD_TASK:
         this._tasksModel.addTask(updateType, update);
@@ -110,6 +116,11 @@ export default class Board {
         break;
       case UpdateType.MAJOR:
         this._clearBoard({resetRenderedTaskCount: true, resetSortType: true});
+        this._renderBoard();
+        break;
+      case UpdateType.INIT:
+        this._isLoading = false;
+        remove(this._loadingComponent);
         this._renderBoard();
         break;
     }
@@ -157,6 +168,10 @@ export default class Board {
     tasks.forEach((task) => this._renderTask(task));
   }
 
+  _renderLoading() {
+    render(this._boardComponent, this._loadingComponent, RenderPosition.AFTERBEGIN);
+  }
+
   _renderNoTasks() {
     render(this._boardComponent, this._noTaskComponent, RenderPosition.AFTERBEGIN);
   }
@@ -185,6 +200,7 @@ export default class Board {
 
     remove(this._sortingComponent);
     remove(this._noTaskComponent);
+    remove(this._loadingComponent);
     remove(this._loadMoreButtonComponent);
 
     if (resetRenderedTaskCount) {
@@ -199,6 +215,11 @@ export default class Board {
   }
 
   _renderBoard() {
+    if (this._isLoading) {
+      this._renderLoading();
+      return;
+    }
+
     const tasks = this._getTasks();
     const taskCount = tasks.length;
 
