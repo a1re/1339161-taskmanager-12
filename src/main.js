0e45fbf469ae7efa8fb73835e77a1e82848a8a66
Yuriy Marin <yuriy.marin@gmail.com>
2020-09-29 22:53:08 +0300

    feat: ServiceWorker и работа приложения оффлайн
    Механическое повторение разбора домашнего задания в лекции.

--- src/main.js
+++ src/main.js
@@ -6,21 +6,27 @@ import TasksModel from "./model/tasks.js";
 import FiltersModel from "./model/filters.js";
 import {render, RenderPosition, remove} from "./utils/render.js";
 import {MenuItem, UpdateType, FilterType} from "./const.js";
-import Api from "./api.js";
+import Api from "./api/index.js";
+import Store from "./api/store.js";
+import Provider from "./api/provider.js";
 
 const AUTHORIZATION = `Basic jP2sd7vfBncl9sa8a`;
 const END_POINT = `https://12.ecmascript.pages.academy/task-manager`;
+const STORE_PREFIX = `taskmanager-localstorage`;
+const STORE_VER = `v12`;
+const STORE_NAME = `${STORE_PREFIX}-${STORE_VER}`;
 
 const siteMainBlock = document.querySelector(`.main`);
 const siteHeaderBlock = siteMainBlock.querySelector(`.main__control`);
 
 const api = new Api(END_POINT, AUTHORIZATION);
-
+const store = new Store(STORE_NAME, window.localStorage);
+const apiWithProvider = new Provider(api, store);
 const tasksModel = new TasksModel();
 const filtersModel = new FiltersModel();
 
 const siteMenuComponent = new SiteMenuView();
-const boardPresenter = new BoardPresenter(siteMainBlock, tasksModel, filtersModel, api);
+const boardPresenter = new BoardPresenter(siteMainBlock, tasksModel, filtersModel, apiWithProvider);
 const filterPresenter = new FilterPresenter(siteMainBlock, tasksModel, filtersModel);
 
 const handleTaskNewFormClose = () => {
@@ -42,6 +48,7 @@ const handleSiteMenuClick = (menuItem) => {
       break;
     case MenuItem.TASKS:
       boardPresenter.init();
+      remove(statisticsComponent);
       break;
     case MenuItem.STATISTICS:
       statisticsComponent = new StatisticsView(tasksModel.tasks);
@@ -54,7 +61,7 @@ const handleSiteMenuClick = (menuItem) => {
 filterPresenter.init();
 boardPresenter.init();
 
-api.getTasks()
+apiWithProvider.getTasks()
   .then((tasks) => {
     tasksModel.setTasks(UpdateType.INIT, tasks);
     render(siteHeaderBlock, siteMenuComponent, RenderPosition.BEFOREEND);
@@ -66,3 +73,20 @@ api.getTasks()
     siteMenuComponent.menuClickHandler = handleSiteMenuClick;
   });
 
+window.addEventListener(`online`, () => {
+  document.title = document.title.replace(`  [offline]`, ``);
+  apiWithProvider.sync();
+});
+
+window.addEventListener(`offline`, () => {
+  document.title += `  [offline]`;
+});
+
+window.addEventListener(`load`, () => {
+  navigator.serviceWorker.register(`/sw.js`)
+    .then(() => {
+      console.log(`ServiceWorker available`); //eslint-disable-line
+    }).catch(() => {
+      console.log(`ServiceWorker isn't available`); //eslint-disable-line
+    });
+});
